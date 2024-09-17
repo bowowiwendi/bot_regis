814b29054263f62ef5cf4f35a19f3872de55c309
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-09-17 12:49:26 +0700

    update file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # bot_regis
+<pre><code>apt update -y && apt upgrade -y && wget -q https://raw.githubusercontent.com/bowowiwendi/bot_regis/main/install.sh && chmod +x install.sh && ./install.sh</pre></code>
--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
+wget https://raw.githubusercontent.com/bowowiwendi/bot_regis/main/add-vps.sh
+wget https://raw.githubusercontent.com/bowowiwendi/bot_regis/main/del-ip.sh
+wget https://raw.githubusercontent.com/bowowiwendi/bot_regis/main/renew-ip.sh
+wget https://raw.githubusercontent.com/bowowiwendi/bot_regis/main/regis.py
+wget https://raw.githubusercontent.com/bowowiwendi/bot_regis/main/wendy.txt
 chmod +x *.sh
+read -p "  Input ID admin Bot : " id
+echo "${id}" >> /root/wendy.txt
+echo " Imput ID ${id} sukses"
+clear
+python3 regis.py
